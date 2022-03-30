21c6aad1b4b97943d00eccf53649204c3f805418
Divesh <owner@Owners-MacBook-Air.local>
2022-03-30 13:21:48 -0400

    Added Events

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ new Vue({
     data: {
         name : 'Divesh',
         job : 'QA',
-        website : 'https://github.com/Divesh23/Vue2Demo'
+        website : 'https://github.com/Divesh23/Vue2Demo',
+        websiteTag: '<a href="https://github.com/">My GitHub Link</a>',
+        age: 25,
+        x: 0,
+        y: 0
     },
     methods: {
         greet : function(time){
             return 'Good ' + time + ', ' + this.name;
-        }   
+        },
+        add : function(inc){
+            this.age += inc;
+        },
+        subtract : function(dec){
+            this.age -= dec;
+        },
+        showCoordinates: function(event){
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        }
     }
 });
