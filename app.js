c6fc6a94c2dbc71b70751548f0fd868dd7f29919
Divesh <owner@Owners-MacBook-Air.local>
2022-03-31 13:52:54 -0400

    Added Preview

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ new Vue({
         websiteTag: '<a href="https://github.com/">My GitHub Link</a>',
         age: 25,
         x: 0,
-        y: 0
+        y: 0,
+        comment:'',
+        a: 0
     },
     methods: {
         greet : function(time){
@@ -32,5 +34,5 @@ new Vue({
         alertDate: function(){
             alert('You Entered Your Date Of Joining');
         },
-    }
+    },
 });
