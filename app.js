780f6852cdcedefb0bf04f235ec4e24a4a8144e4
Divesh <owner@Owners-MacBook-Air.local>
2022-04-03 14:53:42 -0400

    Added fix

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+   
 new Vue({
     el:'#vue-app',
     data: {
@@ -5,15 +7,17 @@ new Vue({
         ended: false
     },
     methods: {
-        punch:function(){
+        punch: function(){
             this.health -= 10;
-            if(this.health <=0){
+            if ( this.health <= 0 ){
                 this.ended = true;
             }
         },
-        restart:function(){
-            this.health =100;
-            this.ended=false;
+        restart: function(){
+            this.health = 100;
+            this.ended = false;
         }
+    },
+    computed: {
     }
 });
