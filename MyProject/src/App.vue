5e9b8929cc199e3327b968245e13431b89c08ad3
Divesh <owner@Owners-MacBook-Air.local>
2022-04-07 08:37:08 -0400

    Init

---
+++ MyProject/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <employee></employee>
+    </div>
+</template>
+
+<script>
+// Imports
+import Employees from './Employees.vue';
+export default {
+    components: {
+        'employee': Employees
+    },
+    data () {
+        return {
+          title: 'Ninja App'
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1{
+  color:red;
+}
+</style>
---
+++ MyProject/src/Employees.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <h1>Employees in the company</h1>
+    <ul>
+        <li v-for="employee in employees" :key="employee">{{ employee }}</li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+          employees: ['Divesh', 'Mario', 'David']
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1{
+    color:green;
+}
+</style>
