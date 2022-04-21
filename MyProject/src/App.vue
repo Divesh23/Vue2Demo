4857dddbd25e52fbdfb6c86d7c42631b6d053c61
Divesh <owner@Owners-MacBook-Air.local>
2022-04-21 17:13:18 -0400

    Init Commit

--- MyProject/src/App.vue
+++ MyProject/src/App.vue
@@ -1,40 +1,21 @@
 <template>
     <div>
-        <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
-        <app-employee v-bind:employees="employees"></app-employee>
-        <app-footer v-bind:title="title"></app-footer>
     </div>
 </template>
 
 <script>
 // Imports
 import Employees from './components/Employees.vue';
-import Footer from './components/Footer.vue';
-import Header from './components/Header.vue';
 export default {
     components: {
-        'app-employee': Employees,
-        'app-footer': Footer,
-        'app-header': Header
     },
     data () {
         return {
-            title: 'Employee App',
-            employees: [
-              {id: 1,name: "John", age: 27,job: "Finance",show: false},
-              {id: 2,name: "Divesh",age: 31,job: "QA Specialist",show: false},
-              {id: 3,name: "David",age: 31,job: "Developer",show: false},
-              {id: 4,name: "Ryan",age: 31,job: "DevOps",show: false},
-              {id: 5,name: "Chris",age: 31,job: "Support",show: false},
-              {id: 6,name: "Taylor",age: 31,job: "Manager",show: false}
-          ]
+            
         }
     },
     methods: {
-        updateTitle:function(updatedTitle){
-            this.title = updatedTitle;
-        }
-
+       
     }
 }
 </script>
