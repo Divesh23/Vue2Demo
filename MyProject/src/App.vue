83649260452d34a7650bb26d5e3458890424aab1
Divesh <owner@Owners-MacBook-Air.local>
2022-04-26 15:20:58 -0400

    Get Requests

--- MyProject/src/App.vue
+++ MyProject/src/App.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
         <add-blog></add-blog>
+        <show-blog></show-blog>
     </div>
 </template>
 
 <script>
 // Imports
 import addBlog from './components/addBlog.vue';
+import showBlog from './components/showBlog.vue';
+
 export default {
     components: {
-        'add-blog': addBlog
+        'add-blog': addBlog,
+        'show-blog': showBlog
     },
     data () {
         return {
---
+++ MyProject/src/components/showBlog.vue
@@ -0,0 +1,50 @@
+<template>
+    <div id="blog"> 
+        <br><br><h1>Blogs</h1><br><br>
+        <div v-for="blog in blogs" :key="blog.title">
+            <ul>
+                <li><h2> Title: {{ blog.title }}</h2>
+                 <p>Body: {{ blog.body }} </p></li>
+            </ul>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            blogs:[]
+        }
+    },
+    methods: {
+    },
+    created() {
+        this.$http.get('http://jsonplaceholder.typicode.com/posts').then(function(data) {
+            this.blogs = data.body.slice(0, 12);
+            console.log(data);
+        });
+
+    }
+}
+</script>
+
+<style scoped>
+#blog {
+    height: 200vh;
+    background: #666666;
+    font-family: 'Balsamiq Sans', sans-serif;
+    width: 200%;
+    max-width: 1200px;
+    margin: 40px auto;
+    padding: 0 20px;
+    box-sizing: border-box;
+    text-align: center;
+}
+
+h1{
+    text-align: center;
+    border: 3px solid black;
+}
+</style>
