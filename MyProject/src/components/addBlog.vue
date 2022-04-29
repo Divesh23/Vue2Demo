3434112803439dd81c24436bf421a599fb5c7011
Divesh <owner@Owners-MacBook-Air.local>
2022-04-28 21:41:40 -0400

    fix for add Blog

--- MyProject/src/components/addBlog.vue
+++ MyProject/src/components/addBlog.vue
@@ -1,31 +1,33 @@
 <template>
     <div id="blog">
-        <form class = card>
+        <form class = "card">
             <h1>The Form</h1>
+            <div v-if="!submitted">
             <label>Enter your Name</label><br>
             <input type="text" required v-model.lazy="blog.name"/> <br><br>
             <label> Enter your content:</label><br>
             <textarea name="texbox" id="" cols="30" rows="10" required v-model.lazy="blog.content"/>
-            <br>
-            <h3>Preview: </h3>
-            <p>Name:{{ blog.name }}</p> 
-            <p>Content:</p>
-            <p>{{ blog.content }}</p>
+            <br><br>
             <div class="checkbox">
                 <label>For Non Paid Viewers</label>
                 <input type="checkbox" value="nonPaid" v-model="blog.categories"/>
                 <label>For Paid Viewers</label>
                 <input type="checkbox" value="Paid" v-model="blog.categories"/>
+            </div><br>
+            <label>Select Any Value for Payment</label>
+            <select v-model="blog.paymentOption">
+            <option v-for="paymentOption in paymentOptions" :key="paymentOption"> {{ paymentOption }}</option>
+            </select>
             </div>
+            <br><br>
+            <h3>Preview: </h3>
+            <p>Name:{{ blog.name }}</p> 
+            <p>Content:</p>
+            <p>{{ blog.content }}</p>
             <p>Checked Values</p>
             <ul>
                 <li v-for="category in blog.categories " :key="category">{{ category }}</li>
             </ul>
-            <br>
-            <label>Select Any Value for Payment</label>
-            <select v-model="blog.paymentOption">
-                <option v-for="paymentOption in paymentOptions" :key="paymentOption"> {{ paymentOption }}</option>
-            </select>
             <p>Payment Option: {{ blog.paymentOption}}</p>
             <button v-on:click.prevent="post">Submit Form</button>
         </form>
